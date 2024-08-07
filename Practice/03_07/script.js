2125e0db984282caec5236c00ae005effbb2dc6f
Vladislav Sytyi <valdis3gs@gmail.com>
2024-08-07 15:07:56 +0200

    first practise task

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,18 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const engine = {
+  type: "V4",
+  size: 1.8,
+  fuel: "Gasoline",
+};
+
+const car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2018,
+  color: "white",
+  engine: engine,
+  features: ["Bluetooth", "Backup Camera", "Cruise Control"],
+};
